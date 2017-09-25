ca0abf84c06c789eadb2d49faffce2ed9052d6f2
yuzmb <744245329@qq.com>
2017-09-25 21:46:31 +0800

    second

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 一个简单的webpack项目
+## 教程 https://segmentfault.com/a/1190000006178770
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: __dirname + '/app/main.js',//入口文件 
   // __dirname 是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
   output: {
-    path: __dirname + '/public',//打包后文件存放的地方
+    path: __dirname + '/build',//打包后文件存放的地方
     filename: 'bundle.js'//打包后输出文件的文件名
   },
   devtool: 'eval-source-map',//调试代码 看错误
   devServer: {
     contentBase: './public',//本地服务器所加载的页面所在的目录
     historyApiFallback: true,//不跳转
-    inline: true//实时刷新
+    inline: true,//实时刷新
+    hot: true
   },
   module: {
     rules: [
@@ -30,9 +34,18 @@ module.exports = {
             options: {
               modules: true
             }
+          }, {
+            loader: 'postcss-loader'
           }
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究'),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/app/index.tmpl.html' //new 一个这个插件的实例，并传入相关的参数
+    }),
+    new webpack.HotModuleReplacementPlugin()//热加载插件
+  ]
 }
